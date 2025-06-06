c1feceda8fd66a073d5eb5d4837a1da8ec0d28ed
yooodleee <accia25@naver.com>
2025-06-07 02:09:24 +0900

    connect to mysql with phpMyAdmin

--- php-apache-app/index.php
+++ php-apache-app/index.php
@@ -1,3 +1,17 @@
 <?php
-echo "Hello from Apache + PHP on Docker!";
+$host = 'db';
+$db = getenv('MYSQL_DATABASE');
+$user = getenv('MYSQL_USER');
+$pass = getenv('MYSQL_PASSWORD');
+
+try {
+    $pdo = new PDO("mysql:host=%host;dbname=%db", $user, $pass);
+    echo "MySQL Connect Success!<br>";
+
+    $stmt = $pdo->query("SELECT NOW() as current_time");
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    echo "current time: " . $row['current_time'];
+} catch (PDOException $e) {
+    echo "MySQL Connect Fail: " . $e->getMessage();
+}
 ?>
